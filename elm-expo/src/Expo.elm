749c4b3ce65b3ca30f3d863b09dde8458af98dae
Mateusz Czapliński <czapkofan@gmail.com>
2018-06-16 19:09:27 +0200

    first draft of a Main app using Expo

--- elm-expo/src/Expo.elm
+++ elm-expo/src/Expo.elm
@@ -1,1 +1,42 @@
 module Expo
+    exposing
+        ( beginnerProgram
+        , Node
+        , text
+        )
+
+import Native.Expo
+
+
+{-| -}
+type Node msg
+    = Node
+
+
+{-| -}
+text : String -> Node msg
+text s =
+    Native.Expo.text s
+
+
+{-| -}
+beginnerProgram :
+    { model : model
+    , view : model -> Node msg
+    , update : msg -> model -> model
+    }
+    -> Program Never model msg
+beginnerProgram =
+    Native.Expo.beginnerProgram
+
+
+{-| -}
+program :
+    { view : model -> Node msg
+    , update : msg -> model -> ( model, Cmd msg )
+    , subscriptions : model -> Sub msg
+    , init : ( model, Cmd msg )
+    }
+    -> Program Never model msg
+program =
+    Native.Expo.program
--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,7 @@
 module Main exposing (..)
 
 import Task
-import NativeUi as Ui exposing (Node)
-import NativeUi.Style as Style exposing (defaultTransform)
-import NativeUi.Elements as Elements exposing (..)
-import NativeUi.Events exposing (..)
-import NativeUi.Image as Image exposing (..)
-import NativeUi.Alert as Alert
+import Expo
 
 
 -- MODEL
@@ -14,14 +9,12 @@ import NativeUi.Alert as Alert
 
 type alias Model =
     { n : Int
-    , failed : String
     }
 
 
 model : Model
 model =
     { n = 9000
-    , failed = "ok"
     }
 
 
@@ -31,44 +24,28 @@ model =
 type Msg
     = Increment
     | Decrement
-    | Alert
-    | Alerted (Result String Bool)
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+-- update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            ( { model | n = model.n + 1 }, Cmd.none )
+            -- ( { model | n = model.n + 1 }, Cmd.none )
+            { model | n = model.n + 1 }
 
         Decrement ->
-            ( { model | n = model.n - 1 }, Cmd.none )
-
-        Alert ->
-            let
-                cmd =
-                    Alert.alert "hello here!" "and there..." []
-                    |> Task.attempt Alerted
-                    -- Task.perform 1 2 (qalert "hi" "there")
-                    -- Task.perform FailedAlert -- Alerted <| 
-                    -- <| qalert "hello heres!" "aind deres..."
-                    -- Alert.alert "hello here!" "and there..." []
-                    -- |> Task.perform (\x -> FailedAlert x) -- (\b -> Alerted b)
-            in
-                ( model, cmd )
-
-        Alerted (Ok _) ->
-            ( { model | failed = "ok alerted" }, Cmd.none )
-
-        Alerted (Err err) ->
-            ( { model | failed = err }, Cmd.none )
+            { model | n = model.n - 1 }
 
 
 -- VIEW
 
 
-view : Model -> Node Msg
+view : Model -> Expo.Node Msg
 view model =
+    Expo.text "hello Elm-Expo!"
+
+{--
     let
         imageSource =
             { uri = "https://raw.githubusercontent.com/futurice/spiceprogram/master/assets/img/logo/chilicorn_no_text-128.png"
@@ -139,6 +116,8 @@ button msg color content =
         ]
         [ Ui.string content ]
 
+--}
+
 
 
 -- PROGRAM
@@ -146,9 +125,14 @@ button msg color content =
 
 main : Program Never Model Msg
 main =
-    Ui.program
-        { init = ( model, Cmd.none )
+    Expo.beginnerProgram
+        { model = model
         , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
         }
+    -- Ui.program
+    --     { init = ( model, Cmd.none )
+    --     , view = view
+    --     , update = update
+    --     , subscriptions = \_ -> Sub.none
+    --     }
