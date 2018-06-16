99597b61d49c28946f12dbfbe5c16266193734e3
Mateusz Czapliński <czapkofan@gmail.com>
2018-06-16 22:34:25 +0200

    WIP cherry-picking RN hijack

--- App.js
+++ App.js
@@ -18,11 +18,34 @@ MessageQueue.spy(info => {
 });
 
 import { NativeModules } from 'react-native'
-NativeModules.DialogManagerAndroid.showAlert(
-  {title: 'hi raw'},
-  errorMessage => console.warn(errorMessage),
-  (action, buttonKey) => { false },
-);
+// import BatchedBridge from 'react-native/Libraries/BatchedBridge/BatchedBridge.js';
+// console.log(`....BB is: ${JSON.stringify(BatchedBridge)}`);
+// var appreg = BatchedBridge.getCallableModule('AppRegistry');
+// console.log(`....appreg is: ${JSON.stringify(appreg)}`);
+
+console.log("...Hijacking RN app...");
+import { AppRegistry } from 'react-native';
+console.log(`....AR is: ${Object.keys(AppRegistry)}`);
+console.log(`....AR.rR is: ${AppRegistry.registerRunnable}`);
+var oldrc = AppRegistry.registerComponent;
+AppRegistry.registerComponent = (a,b,c) => {
+  console.log(`IN RC! ${a} / ${c}`);
+  if (a === 'main') {
+    var newmain = () => {
+      console.log('...hijack called!...');
+      NativeModules.DialogManagerAndroid.showAlert(
+        {title: 'hi raw'},
+        errorMessage => console.warn(errorMessage),
+        (action, buttonKey) => { false },
+      );
+    }
+    // TODO(akavel): choice below seems to depend on RN version? or what?
+    // return AppRegistry.registerRunnable(a, {run: newmain});
+    return AppRegistry.registerRunnable(a, newmain);
+  }
+  return oldrc(a,b,c);
+};
+console.log('...attempted hijack end');
 
 const Elm = require('./elm');
 export default Elm.Main.start();
