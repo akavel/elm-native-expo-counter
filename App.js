f8d9c64fb0c166f3a42675f538a2d725f5379078
Mateusz Czapliński <czapkofan@gmail.com>
2018-06-18 22:50:54 +0200

    improve MessageQueue.spy filtering to show timers

--- App.js
+++ App.js
@@ -2,18 +2,35 @@ import MessageQueue from 'react-native/Libraries/BatchedBridge/MessageQueue.js';
 MessageQueue.spy(info => {
   var name = `${info.module ? info.module + '.' : ''}${info.method}`;
   var exclude = [
-    'RCTDeviceEventEmitter.emit',
+    // 'RCTDeviceEventEmitter.emit',
     'Networking.sendRequest',
-    'Timing.createTimer',
-    'JSTimers.callTimers',
+    // 'Timing.createTimer',
+    // 'JSTimers.callTimers',
     'WebSocketModule.connect',
   ];
   if (!exclude.includes(name)) {
-    // console.log(name);
-    console.log(`${info.type === 0 ? 'N->JS' : 'JS->N'} : ` +
-      `${info.module ? info.module + '.' : ''}${info.method}` +
-      `(${JSON.stringify(info.args).slice(1, -1)})`,
-    );
+    var args = JSON.stringify(info.args).slice(1, -1);
+    var exclude = [
+      'didReceiveNetworkResponse',
+      'didSendNetworkData',
+      'didReceiveNetworkData',
+      'didCompleteNetworkResponse',
+      'websocketFailed',
+    ];
+    var skip = false;
+    for (var i = 0; i < exclude.length; i++) {
+      if (args.includes(exclude[i])) {
+        skip = true;
+        break;
+      }
+    }
+    if (!skip) {
+      // console.log(name);
+      console.log(`${info.type === 0 ? 'N->JS' : 'JS->N'} : ` +
+        `${info.module ? info.module + '.' : ''}${info.method}` +
+        `(${JSON.stringify(info.args).slice(1, -1)})`,
+      );
+    }
   }
 });
 
