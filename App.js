a9140d6c77bc699527c7e743da9669ad00881518
Mateusz Czapliński <czapkofan@gmail.com>
2018-06-20 20:36:52 +0200

    App.js: cleanup a bit

--- App.js
+++ App.js
@@ -34,45 +34,10 @@ MessageQueue.spy(info => {
   }
 });
 
-// document = 'Hello doc!';
 var elmrn = require('./hijack0');
 elmrn.prepare();
 const Elm = require('./elm');
 elmrn.bridge(Elm.Main);
 
-/*
-import { NativeModules } from 'react-native'
-
-// console.log(`...RN is: ${Object.keys(require('react-native')).sort()}`);
-
-console.log("...Hijacking RN app...");
-import { AppRegistry } from 'react-native';
-import BatchedBridge from 'react-native/Libraries/BatchedBridge/BatchedBridge.js';
-// console.log(`....AR is: ${Object.keys(AppRegistry)}`);
-// console.log(`....AR.rR is: ${AppRegistry.registerRunnable}`);
-var oldrc = AppRegistry.registerComponent;
-AppRegistry.registerComponent = (a,b,c) => {
-  // console.log(`IN RC! ${a} / ${c}`);
-  if (a === 'main') {
-    var newmain = (appParameters) => {
-      console.log('...hijack called!...');
-      var rcevt = BatchedBridge.getCallableModule('RCTEventEmitter');
-      // console.log(`...BB.gCM RCTEE = ${Object.keys(rcevt).sort()}`);
-      // console.log(`...BB.gCM RCTEE = ${JSON.stringify(rcevt)}`);
-      Elm.Main.run(appParameters);
-      NativeModules.DialogManagerAndroid.showAlert(
-        {title: 'hi raw'},
-        errorMessage => console.warn(errorMessage),
-        (action, buttonKey) => { false },
-      );
-    }
-    // TODO(akavel): choice below seems to depend on RN version? or what?
-    // return AppRegistry.registerRunnable(a, {run: newmain});
-    return AppRegistry.registerRunnable(a, newmain);
-  }
-  return oldrc(a,b,c);
-};
-console.log('...attempted hijack end');
-*/
 console.log('...app.js end...');
 
